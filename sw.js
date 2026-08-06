a90b1bf022410eea33f8fbb0df1a38ab30f800f6
jove7913 <jove7913@gmail.com>
2026-08-06 08:45:39 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "labelverify-v5";
+const CACHE = "labelverify-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png",
   "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/umd/supabase.min.js"];
 
